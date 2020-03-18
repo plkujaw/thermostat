d2cc5126954d8d604aa9643fe9746ab195918f89
plkujaw <jawor.jakub@gmail.com>
2020-03-18 13:59:29 +0000

    setup first test

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,1 +1,16 @@
 'use strict'
+
+describe('Thermostat', function() {
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  it('Is set to 20 degrees by default', function() {
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+
+
+});
